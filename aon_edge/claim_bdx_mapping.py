7fc93189707b61cfc5dfea296bcbb8a7562ac62d
ReturnoftheMak <50878639+ReturnoftheMak@users.noreply.github.com>
2020-02-17 14:26:52 +0000

    claims cleaning function nearly written

--- aon_edge/claim_bdx_mapping.py
+++ aon_edge/claim_bdx_mapping.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 # %% Get a dict of col name to mapped name
 
-
+filename_claims = r'\\svrtcs04\Syndicate Data\Actuarial\Pricing\2_Account_Pricing\NFS_Edge\Knowledge\Data_Received\Monthly\_ColumnMapping\claim_WITHACTIONS.xlsx'
 
 def get_mapping(filename):
     """Get a dictionary of column mapping, and a list of the column subset that we want to retain.
@@ -18,12 +18,10 @@ def get_mapping(filename):
 
     mapping_dict = dict(zip(keys, values))
 
-    cols_required = list(df_claim_mapping[df_claim_mapping['Need?'] == 'Y']['Rename'])
-
-    return mapping_dict, cols_required
+    return mapping_dict
 
 
-def claims_bdx_clean(file, mapping_dict, cols_required):
+def claims_bdx_clean(file, mapping_dict):
     """Read in file, change column headers and output only the required cols
     """
 
@@ -33,8 +31,23 @@ def claims_bdx_clean(file, mapping_dict, cols_required):
 
     df = df[[col for col in df.columns if type(col) is not float]]
 
-    # Implement Checks now
+    # Implement Checks
 
-    df = df.drop(labels=['Name_Claimant', 'Name_Insured'])
+    # GDPR drop fields
+    df = df.drop(labels=['Name_Claimant', 'Name_Insured'], axis=1)
+
+    # Total checks on Incurred
+    incurred_check = round(df.Incurred,0) == round(df.Incurred_Indemnity + df.Incurred_Expenses + df.Incurred_TPA_Fees, 0)
+
+    df_false = df[~incurred_check]
+
+    # Need to also check on the policy ID at some point
+
+    if len(df_false) > 0:
+        print('Output rejected due to {0} rows not matching on Incurred values'.format(len(df_false)))
+    else:
+        return df
 
 
+
+# %%
